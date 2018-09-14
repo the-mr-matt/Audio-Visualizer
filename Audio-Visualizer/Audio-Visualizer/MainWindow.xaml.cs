5d97c64a8398db556fde7f419a4917390c8f3a83
Matthew Inglis <matthew@winglett.co.nz>
2018-09-14 20:24:55 +1200

    Basic UI and window control

---
+++ Audio-Visualizer/Audio-Visualizer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Audio_Visualizer.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Audio_Visualizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void close_window_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHandling.CloseWindow(this);
+        }
+
+        private void maximize_window_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                WindowHandling.RestoreWindow(this);
+            }
+            else
+            {
+                WindowHandling.MaximizeWindow(this);
+            }
+        }
+
+        private void minimize_window_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHandling.MinimizeWindow(this);
+        }
+    }
+}
---
+++ Audio-Visualizer/Audio-Visualizer/UI/WindowHandling.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace Audio_Visualizer.UI
+{
+    class WindowHandling
+    {
+        /// <summary>
+        /// Closes the application on click, mimics the X button on a normal window
+        /// </summary>
+        public static void CloseWindow(Window window)
+        {
+            SystemCommands.CloseWindow(window);
+        }
+
+        /// <summary>
+        /// Maximizes the window, mimics the center button at the top of a normal window
+        /// </summary>
+        public static void MaximizeWindow(Window window)
+        {
+            SystemCommands.MaximizeWindow(window);
+        }
+
+        /// <summary>
+        /// Minimizes the window, mimics the left button on a normal window
+        /// </summary>
+        public static void MinimizeWindow(Window window)
+        {
+            SystemCommands.MinimizeWindow(window);
+        }
+
+        /// <summary>
+        /// Restores the window
+        /// </summary>
+        public static void RestoreWindow(Window window)
+        {
+            SystemCommands.RestoreWindow(window);
+        }
+    }
+}
