f3cf31f6eb8e5f33bd529018e0ca72c79eef44de
Matthew Inglis <matthew@winglett.co.nz>
2018-09-17 07:52:43 +1200

    UI

--- Audio-Visualizer/Audio-Visualizer/MainWindow.xaml.cs
+++ Audio-Visualizer/Audio-Visualizer/MainWindow.xaml.cs
@@ -24,28 +24,87 @@ namespace Audio_Visualizer
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
+        #region WindowCommands
         private void close_window_Click(object sender, RoutedEventArgs e)
         {
             WindowHandling.CloseWindow(this);
         }
-
         private void maximize_window_Click(object sender, RoutedEventArgs e)
         {
-            if(this.WindowState == WindowState.Maximized)
+            //set the window state and disable window resizing when the window is maximized
+            if(WindowState == WindowState.Maximized)
             {
                 WindowHandling.RestoreWindow(this);
+                //WindowHandling.EnableResize(WindowResize);
             }
             else
             {
                 WindowHandling.MaximizeWindow(this);
+                //WindowHandling.DisableResize(WindowResize);
             }
         }
-
         private void minimize_window_Click(object sender, RoutedEventArgs e)
         {
             WindowHandling.MinimizeWindow(this);
         }
+        private void window_drag(object sender, MouseButtonEventArgs e)
+        {
+            WindowHandling.DragWindow(this);
+        }
+        #endregion
+
+        #region WindowResize
+        private void ResizeInit(object sender, MouseButtonEventArgs e)
+        {
+            WindowHandling.StartResize(sender);
+        }
+        private void ResizeEnd(object sender, MouseButtonEventArgs e)
+        {
+            WindowHandling.EndResize(sender);
+        }
+        private void ResizingWindow(object sender, MouseEventArgs e)
+        {
+            WindowHandling.ResizeWindow(sender, e);
+        }
+        #endregion
+        
+        #region MenuNavigation
+        private void select_mixer(object sender, RoutedEventArgs e)
+        {
+            SetIconPosition(Mixer);
+        }
+        private void select_eq(object sender, RoutedEventArgs e) 
+        {
+            SetIconPosition(EQ);
+        }
+        private void select_chroma(object sender, RoutedEventArgs e)
+        {
+            SetIconPosition(Chroma);
+        }
+        private void select_comms(object sender, RoutedEventArgs e)
+        {
+            SetIconPosition(Comms);
+        }
+        private void select_settings(object sender, RoutedEventArgs e)
+        {
+            SetIconPosition(Settings);
+        }
+
+        /// <summary>
+        /// Positions the selected panel icon next to the button that was pressed
+        /// </summary>
+        private void SetIconPosition(Button button)
+        {
+            //get the margin of the button and half it to make the margin relative to the center of the button
+            //subtract half the height of the icon to make the margin relative to the center of the icon
+
+            var margin = SelectedPanelIcon.Margin;
+            margin.Top = button.Margin.Top + button.Height / 2f - SelectedPanelIcon.Height / 2f;
+            SelectedPanelIcon.Margin = margin;
+        }
+        #endregion
     }
 }
--- Audio-Visualizer/Audio-Visualizer/UI/WindowHandling.cs
+++ Audio-Visualizer/Audio-Visualizer/UI/WindowHandling.cs
@@ -1,9 +1,18 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace Audio_Visualizer.UI
 {
     class WindowHandling
     {
+        #region Variables
+        private static bool currentlyResizing;
+        const int increment = 5;
+        #endregion
+
+        #region WindowCommands
         /// <summary>
         /// Closes the application on click, mimics the X button on a normal window
         /// </summary>
@@ -35,5 +44,107 @@ namespace Audio_Visualizer.UI
         {
             SystemCommands.RestoreWindow(window);
         }
+
+        /// <summary>
+        /// Allows the window to be dragged from specific elements - the main window is hidden so this needs to be manually handled
+        /// </summary>
+        public static void DragWindow(Window window)
+        {
+            window.DragMove();
+        }
+        #endregion
+
+        #region WindowResize
+        /// <summary>
+        /// Initialize the window resizing
+        /// </summary>
+        public static void StartResize(object sender)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                currentlyResizing = true;
+                senderRect.CaptureMouse();
+            }
+        }
+
+        /// <summary>
+        /// End the window resizing
+        /// </summary>
+        public static void EndResize(object sender)
+        {
+            Rectangle senderRect = sender as Rectangle;
+            if (senderRect != null)
+            {
+                currentlyResizing = false;
+                senderRect.ReleaseMouseCapture();
+            }
+        }
+
+        /// <summary>
+        /// Resizes the window based on the rectangle that was dragged
+        /// </summary>
+        public static void ResizeWindow(object sender, MouseEventArgs e)
+        {
+            if (currentlyResizing)
+            {
+                Rectangle senderRect = sender as Rectangle;
+                Window mainWindow = senderRect.Tag as Window;
+                if (senderRect != null)
+                {
+                    double width = e.GetPosition(mainWindow).X;
+                    double height = e.GetPosition(mainWindow).Y;
+
+                    senderRect.CaptureMouse();
+
+                    if (senderRect.Name.ToLower().Contains("right"))
+                    {
+                        width += increment;
+                        if (width > 0) { mainWindow.Width = width; }
+                    }
+
+                    if (senderRect.Name.ToLower().Contains("left"))
+                    {
+                        width -= increment;
+                        mainWindow.Left += width;
+                        width = mainWindow.Width - width;
+                        
+                        if (width > 0) { mainWindow.Width = width; }
+                    }
+
+                    if (senderRect.Name.ToLower().Contains("bottom"))
+                    {
+                        height += increment;
+                        if (height > 0) { mainWindow.Height = height; }
+                    }
+
+                    if (senderRect.Name.ToLower().Contains("top"))
+                    {
+                        height -= increment;
+                        mainWindow.Top += height;
+                        height = mainWindow.Height - height;
+                        if (height > 0) { mainWindow.Height = height; }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows the resizing rectangles
+        /// </summary>
+        public static void EnableResize(Grid grid)
+        {
+            grid.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Hides the resizing rectangles
+        /// </summary>
+        public static void DisableResize(Grid grid)
+        {
+
+            grid.IsEnabled = false;
+        }
+        #endregion
     }
 }
